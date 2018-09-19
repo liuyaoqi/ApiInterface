c7b99914e61cc58801c41142c03285f29c3d320a
liuyaoqi <1660896176@qq.com>
2018-09-19 14:20:54 +0800

    数据标准化输出完毕

--- client/client.php
+++ client/client.php
@@ -1,3 +1,21 @@
 <?php  
 	//客户端发起api请求 
+	include './vendor/autoload.php';
+
+	$curl = new Curl\Curl();
+
+	//请求的api地址
+
+	$url = 'http://localhost/ApiInterface/server/server.php';
+	
+	$curl->get($url);
+
+	//是否成功
+	if ($curl->error) {
+	    echo $curl->error_code;
+	}
+	else {
+	    echo $curl->response;
+	}
+
 ?>
--- server/server.php
+++ server/server.php
@@ -1,3 +1,28 @@
 <?php 
 	//接收客户端发来的请求 响应数据 json格式
+	
+	$pdo = new PDO('mysql:host=localhost;dbname=api;charset=utf8','root','');
+
+	$stmt = $pdo->query('select * from user');
+
+	$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	
+	//定义标准化的数据格式
+	function resp($data,$status,$message){
+		
+		$res = [
+			'status'=>$status,//服务器响应状态码 200 401 404
+			'message'=>$message,//此次api请求的描述
+			'data'=>$data,
+		];
+
+		return json_encode($res,JSON_UNESCAPED_UNICODE);
+	}	
+
+	echo resp($data,200,'pk');
+
+	
+
+	
 ?>
